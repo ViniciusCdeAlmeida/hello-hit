1ea6d80adf98b0ad18887377202889ccfa82e978
adryelklever <adryelklever3m@hotmail.com>
2021-01-27 16:44:40 -0300

    Screen Conversas e Chat

---
+++ lib/screens/chat/chat_screen.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreen createState() => _ChatScreen();
+}
+
+class _ChatScreen extends State<ChatScreen> {
+  List<String> listaMensagens = [
+    "Olá meu amigo, tudo bem?",
+    "Tudo ótimo!!! e contigo?",
+    "Estou muito bem!! queria ver uma coisa contigo, você vai na corrida de sábado?",
+    "Não sei ainda :(",
+    "Pq se você fosse, queria ver se posso ir com você...",
+    "Posso te confirma no sábado? vou ver isso",
+    "Opa! tranquilo",
+    "Excelente!!",
+    "Estou animado para essa corrida, não vejo a hora de chegar! ;) ",
+    "Vai estar bem legal!! muita gente",
+    "vai sim!",
+    "Lembra do carro que tinha te falado",
+    "Que legal!!"
+  ];
+  TextEditingController _controllerMensagem = TextEditingController();
+
+  _enviarMensagem() {}
+
+  _salvarMensagem() async {}
+
+  _enviarFoto() {}
+
+  _recuperarDadosUsuario() async {}
+
+  @override
+  void initState() {
+    super.initState();
+    _recuperarDadosUsuario();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var caixaMensagem = Container(
+      padding: EdgeInsets.all(8),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(right: 8),
+              child: TextField(
+                //controller: _controllerMensagem,
+                autofocus: true,
+                keyboardType: TextInputType.text,
+                style: TextStyle(fontSize: 15),
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.fromLTRB(32, 8, 32, 8),
+                  hintText: "Digite uma mensagem...",
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  prefixIcon: IconButton(
+                      icon: Icon(Icons.camera_alt), onPressed: _enviarFoto),
+                ),
+              ),
+            ),
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.orange[700],
+            child: Icon(
+              Icons.send,
+              color: Colors.white,
+            ),
+            mini: true,
+            onPressed: _enviarMensagem,
+          )
+        ],
+      ),
+    );
+
+    var listView = Expanded(
+      child: ListView.builder(
+          itemCount: listaMensagens.length,
+          itemBuilder: (context, indice) {
+            double larguraContainer = MediaQuery.of(context).size.width * 0.8;
+
+            //Define cores e alinhamentos
+            Alignment alinhamento = Alignment.centerRight;
+            Color cor = Colors.orange[700];
+            if (indice % 2 == 0) {
+              //par
+              alinhamento = Alignment.centerLeft;
+              cor = Colors.black;
+            }
+
+            return Align(
+              alignment: alinhamento,
+              child: Padding(
+                padding: EdgeInsets.all(6),
+                child: Container(
+                  width: larguraContainer,
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: cor,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                  ),
+                  child: Text(
+                    listaMensagens[indice],
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+            );
+          }),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_sharp,
+            color: Colors.orange[700],
+          ),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: 8),
+              child: Text('Marke Strong ||'),
+            )
+          ],
+        ),
+        actions: [
+          Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: Icon(
+                  Icons.add,
+                  color: Colors.orange[700],
+                  size: 26.0,
+                ),
+              )),
+        ],
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        /*decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/social.png"), fit: BoxFit.cover),
+        ),*/
+        child: SafeArea(
+            child: Container(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: <Widget>[
+              listView,
+              caixaMensagem,
+            ],
+          ),
+        )),
+      ),
+    );
+  }
+}
