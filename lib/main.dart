bc5dda338e00d1fef869e65c1b8cea8dcf9cebc2
adryelklever <adryelklever3m@hotmail.com>
2021-02-01 00:07:21 -0300

    Atualização de telas

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:provider/provider.dart';
 import 'package:hellohit/utils/providers.dart';
 import 'package:hellohit/utils/rotas.dart';
 
-import 'package:hellohit/screens/postagem/postagem_screen.dart';
+import 'package:hellohit/screens/camera_post/camera_post.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
           errorColor: const Color(0xFF7A1C02),
           fontFamily: 'Lato',
         ),
-        home: PostagemScreen(),
+        home: CameraPostScreen(),
         routes: rotas(),
       ),
     );
