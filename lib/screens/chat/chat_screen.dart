7eafac295a4535277caf9019e26573e68a8b6403
adryelklever <adryelklever3m@hotmail.com>
2021-01-31 22:50:07 -0300

    Atualização de telas

--- lib/screens/chat/chat_screen.dart
+++ lib/screens/chat/chat_screen.dart
@@ -1,169 +1,170 @@
 import 'package:flutter/material.dart';
+import 'package:hellohit/models/message_model.dart';
+import 'package:hellohit/models/usuario_model.dart';
 
 class ChatScreen extends StatefulWidget {
+  final Usuario user;
+
+  ChatScreen({this.user});
+
   @override
-  _ChatScreen createState() => _ChatScreen();
+  _ChatScreenState createState() => _ChatScreenState();
 }
 
-class _ChatScreen extends State<ChatScreen> {
-  List<String> listaMensagens = [
-    "Olá meu amigo, tudo bem?",
-    "Tudo ótimo!!! e contigo?",
-    "Estou muito bem!! queria ver uma coisa contigo, você vai na corrida de sábado?",
-    "Não sei ainda :(",
-    "Pq se você fosse, queria ver se posso ir com você...",
-    "Posso te confirma no sábado? vou ver isso",
-    "Opa! tranquilo",
-    "Excelente!!",
-    "Estou animado para essa corrida, não vejo a hora de chegar! ;) ",
-    "Vai estar bem legal!! muita gente",
-    "vai sim!",
-    "Lembra do carro que tinha te falado",
-    "Que legal!!"
-  ];
-  TextEditingController _controllerMensagem = TextEditingController();
+class _ChatScreenState extends State<ChatScreen> {
+  _buildMessage(Message message, bool isMe) {
+    final Container msg = Container(
+      margin: isMe
+          ? EdgeInsets.only(
+              top: 8.0,
+              bottom: 8.0,
+              left: 80.0,
+            )
+          : EdgeInsets.only(
+              top: 8.0,
+              bottom: 8.0,
+            ),
+      padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+      width: MediaQuery.of(context).size.width * 0.75,
+      decoration: BoxDecoration(
+        color: isMe ? Colors.orange[700] : Colors.black,
+        borderRadius: isMe
+            ? BorderRadius.only(
+                topLeft: Radius.circular(15.0),
+                bottomLeft: Radius.circular(15.0),
+              )
+            : BorderRadius.only(
+                topRight: Radius.circular(15.0),
+                bottomRight: Radius.circular(15.0),
+              ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            message.time,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            message.text,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+    if (isMe) {
+      return msg;
+    }
+    return Row(
+      children: <Widget>[
+        msg,
+        IconButton(
+          icon: message.isLiked
+              ? Icon(Icons.favorite)
+              : Icon(Icons.favorite_border),
+          iconSize: 30.0,
+          color:
+              message.isLiked ? Theme.of(context).primaryColor : Colors.black,
+          onPressed: () {},
+        )
+      ],
+    );
+  }
 
-  _enviarMensagem() {}
-
-  _salvarMensagem() async {}
-
-  _enviarFoto() {}
-
-  _recuperarDadosUsuario() async {}
-
-  @override
-  void initState() {
-    super.initState();
-    _recuperarDadosUsuario();
+  _buildMessageComposer() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      height: 70.0,
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.photo),
+            iconSize: 25.0,
+            color: Theme.of(context).primaryColor,
+            onPressed: () {},
+          ),
+          Expanded(
+            child: TextField(
+              textCapitalization: TextCapitalization.sentences,
+              onChanged: (value) {},
+              decoration: InputDecoration.collapsed(
+                hintText: 'Send a message...',
+              ),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.send),
+            iconSize: 25.0,
+            color: Theme.of(context).primaryColor,
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    var caixaMensagem = Container(
-      padding: EdgeInsets.all(8),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.only(right: 8),
-              child: TextField(
-                //controller: _controllerMensagem,
-                autofocus: true,
-                keyboardType: TextInputType.text,
-                style: TextStyle(fontSize: 15),
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.fromLTRB(32, 8, 32, 8),
-                  hintText: "Digite uma mensagem...",
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(32),
-                  ),
-                  prefixIcon: IconButton(
-                      icon: Icon(Icons.camera_alt), onPressed: _enviarFoto),
-                ),
-              ),
-            ),
-          ),
-          FloatingActionButton(
-            backgroundColor: Colors.orange[700],
-            child: Icon(
-              Icons.send,
-              color: Colors.white,
-            ),
-            mini: true,
-            onPressed: _enviarMensagem,
-          )
-        ],
-      ),
-    );
-
-    var listView = Expanded(
-      child: ListView.builder(
-          itemCount: listaMensagens.length,
-          itemBuilder: (context, indice) {
-            double larguraContainer = MediaQuery.of(context).size.width * 0.8;
-
-            //Define cores e alinhamentos
-            Alignment alinhamento = Alignment.centerRight;
-            Color cor = Colors.orange[700];
-            if (indice % 2 == 0) {
-              //par
-              alinhamento = Alignment.centerLeft;
-              cor = Colors.black;
-            }
-
-            return Align(
-              alignment: alinhamento,
-              child: Padding(
-                padding: EdgeInsets.all(6),
-                child: Container(
-                  width: larguraContainer,
-                  padding: EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: cor,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(8),
-                    ),
-                  ),
-                  child: Text(
-                    listaMensagens[indice],
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ),
-              ),
-            );
-          }),
-    );
     return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
       appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back_sharp,
-            color: Colors.orange[700],
+        centerTitle: true,
+        title: Text(
+          widget.user.nome,
+          style: TextStyle(
+            fontSize: 28.0,
+            fontWeight: FontWeight.bold,
           ),
-          onPressed: () => Navigator.of(context).pop(),
         ),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(left: 8),
-              child: Text('Marke Strong ||'),
-            )
-          ],
-        ),
-        actions: [
-          Padding(
-              padding: EdgeInsets.only(right: 20.0),
-              child: GestureDetector(
-                onTap: () {},
-                child: Icon(
-                  Icons.add,
-                  color: Colors.orange[700],
-                  size: 26.0,
-                ),
-              )),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            iconSize: 30,
+            color: Colors.white,
+            onPressed: () {},
+          ),
         ],
       ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        /*decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("assets/images/social.png"), fit: BoxFit.cover),
-        ),*/
-        child: SafeArea(
-          child: Container(
-            padding: EdgeInsets.all(8),
-            child: Column(
-              children: <Widget>[
-                listView,
-                caixaMensagem,
-              ],
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  ),
+                  child: ListView.builder(
+                    reverse: true,
+                    padding: EdgeInsets.only(top: 15.0),
+                    itemCount: messages.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final Message message = messages[index];
+                      final bool isMe = message.sender.id == currentUser.id;
+                      return _buildMessage(message, isMe);
+                    },
+                  ),
+                ),
+              ),
             ),
-          ),
+            _buildMessageComposer(),
+          ],
         ),
       ),
     );
